f41bca1a4c99747bf2a90422fc4eb647c17eca94
Olivia Pomeroy <opomeroy26@gmail.com>
2022-02-06 19:29:25 -0800

    commit tree 29dcb7385b6aefed92a9dfe6aad3689f7d10621e

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(e) {
+   let words= document.querySelector('p').textContent = "This is really cool!";
+   console.log(words);
+})
+
+
